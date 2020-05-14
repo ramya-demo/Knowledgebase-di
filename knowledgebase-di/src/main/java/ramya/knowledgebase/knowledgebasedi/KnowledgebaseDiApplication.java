d38f2ebbabcb3cf97216ec3e7efd39c9e40eae57
ragu <ragu@host.docker.internal>
2020-05-14 08:43:59 +0530

    Fisrt Commit for DI

---
+++ knowledgebase-di/src/main/java/ramya/knowledgebase/knowledgebasedi/KnowledgebaseDiApplication.java
@@ -0,0 +1,13 @@
+package ramya.knowledgebase.knowledgebasedi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KnowledgebaseDiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KnowledgebaseDiApplication.class, args);
+	}
+
+}
